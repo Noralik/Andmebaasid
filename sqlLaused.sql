1922cd4fddfd780baa08ab4e050e8d55ec7cd83a
Noralik <158551209+Noralik@users.noreply.github.com>
2025-04-07 09:35:02 +0300

    Create Database and table

--- sqlLaused.sql
+++ sqlLaused.sql
@@ -6,15 +6,29 @@
   -- SQL Server Auth -- varem loodud kasutajad
 -- New Query
 --
---
---
---
---
---
---
---
---
---
---
---
---
+CREATE DATABASE gutsuljak;
+--Object Explorer on vaja pidevalt uuendada käsitsi!
+USE gutsuljak;
+--tabeli loomine
+CREATE TABLE oplilane(
+	opilane_ID int Primary Key identity(1,1),
+	eesnimi varchar(25),
+	perenimi varchar(25) Unique,
+	synniaeg date,
+	aadress TEXT,
+	opilaskodu bit
+);
+-- select table
+SELECT * FROM oplilane;
+
+--tabeli kustutamine 
+DROP table oplilane;
+
+--andmete lisamine tabelisse
+INSERT INTO oplilane(eesnimi, perenimi, synniaeg, aadress, opilaskodu)
+VALUES 
+    ('Gerald', 'Rodger', '1211-12-08', 'Rivia', '1'),
+    ('Steve', 'Sigma', '2011-12-19', 'Minecraft', '0'),
+    ('Lux', 'light', '2015-07-14', 'Demacia', '0'),
+    ('Herobrine', 'Hero', '2013-12-19', 'Minecraft', '0'),
+    ('Irelia', 'sword', '2017-07-14', 'Demacia', '0');
